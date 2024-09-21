5463f9a8ceb673ff774c878c7296338ae063249f
Keerthana Deiveehan <keerthideiveehan@gmail.com>
2024-09-21 13:39:33 -0600

    test

---
+++ 0_basics/Calc.java
@@ -0,0 +1,10 @@
+public class Calc {
+
+    public static void main(String[] args) {
+        System.out.println("1+1=2  keertyji");
+    }
+
+    public void greet(){
+        System.out.println("hey");
+    }
+}
